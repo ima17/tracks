c9a2ff5fc91beee48769d000d89b62a3c3d848dc
ima17 <ima.sudaraka@outlook.com>
2023-03-06 23:18:12 +0530

    Add UI changes

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { Text, Input, Button } from "@rneui/themed";
 import Spacer from "../components/spacer";
 import { Context as AuthContext } from "../context/authContext";
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
   const { signup, state } = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -36,6 +36,13 @@ const SignupScreen = () => {
       <Spacer>
         <Button title="Signup" onPress={() => signup({ email, password })} />
       </Spacer>
+      <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
+        <Spacer>
+          <Text style={styles.link}>
+            Already have an account? Sign In instead.
+          </Text>
+        </Spacer>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -51,6 +58,9 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     color: "red",
     fontSize: 16,
+  },
+  link: {
+    color: "blue",
   },
 });
 
