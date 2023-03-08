ceca8c00c983ff9eaac1e8ad8e651bfb1f14d70a
ima17 <ima.sudaraka@outlook.com>
2023-03-09 00:07:34 +0530

    Clear error on navigation in signin screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -3,12 +3,20 @@ import { View, StyleSheet } from "react-native";
 import { Context as AuthContext } from "../context/authContext";
 import AuthForm from "../components/AuthForm";
 import Navilink from "../components/NavLink";
+import { NavigationEvents } from "@react-navigation/compat";
 
-const SignupScreen = () => {
-  const { signin, state } = useContext(AuthContext);
+const SignupScreen = ({ navigation }) => {
+  const { signin, state, clearErrorMessage } = useContext(AuthContext);
+
+  React.useEffect(() => {
+    navigation.addListener("blur", () => {
+      clearErrorMessage();
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
+      <NavigationEvents onWillBlur={clearErrorMessage} />
       <AuthForm
         headerText="Sign In to Your Account"
         errorMessage={state.errorMessage}
