a76507c6a8568ce31f34577a3b818a5bc494fbc4
ima17 <ima.sudaraka@outlook.com>
2023-03-11 00:10:51 +0530

    Create map component

---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "@rneui/themed";
+import { StyleSheet } from "react-native";
+
+const Map = () => {
+  return <Text>Map</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default Map;
