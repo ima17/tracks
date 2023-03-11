5a7fe47b02690c8b91baf4346e511b62470ca729
ima17 <ima.sudaraka@outlook.com>
2023-03-11 15:35:41 +0530

    Resolve version issue

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://30fc-2402-4000-2082-54e7-e590-6683-f21b-75c8.ap.ngrok.io",
+  baseURL: "https://a1df-103-21-164-41.in.ngrok.io",
 });
