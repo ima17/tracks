70974dc4ba583315670f419253d851cf144eb32e
ima17 <ima.sudaraka@outlook.com>
2023-03-14 22:34:12 +0530

    Add UI changes

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const TrackDetailScreen = () => {
   return (
-    <View>
+    <SafeAreaView forceInset={{ top: "always" }}>
       <Text>Track Detail Screen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const TrackListScreen = () => {
   return (
-    <View>
+    <SafeAreaView forceInset={{ top: "always" }}>
       <Text>Track List Screen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
