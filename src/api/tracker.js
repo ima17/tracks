6e4e9a2ac1fa0326fffd7371d7ff47088456f0ef
ima17 <ima.sudaraka@outlook.com>
2023-03-23 19:42:43 +0530

    Upadate api call

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: " https://3565-2402-4000-2081-66f2-d41c-dfe9-2d3-cef9.ap.ngrok.io",
+  baseURL: "https://0b3d-2402-4000-2081-66f2-d904-2b74-eece-e0e9.in.ngrok.io",
 });
