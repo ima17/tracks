5e421cb123fa51502af26335128d46d9a4be6046
ima17 <ima.sudaraka@outlook.com>
2023-03-24 17:12:09 +0530

    Update tracker

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://0b3d-2402-4000-2081-66f2-d904-2b74-eece-e0e9.in.ngrok.io",
+  baseURL: "https://e319-103-21-166-8.in.ngrok.io",
 });
