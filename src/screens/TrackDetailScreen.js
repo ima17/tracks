9dac9e76e085d3fe2e8727be4311b755c5bbe8ba
ima17 <ima.sudaraka@outlook.com>
2023-03-27 00:23:52 +0530

    Show track details

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,21 +1,38 @@
 import React, { useContext } from "react";
 import { Text, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { Context as TrackContext } from "../context/trackContext";
+import MapView, { Polyline } from "react-native-maps";
 
 const TrackDetailScreen = ({ route }) => {
   const { state } = useContext(TrackContext);
   const { _id } = route.params;
 
-  const track = state.find((t) => t._id === _id);
+  const tracks = state.find((t) => t._id === _id);
+  const initialCoords = tracks.locations[0].coords;
 
   return (
-    <SafeAreaView forceInset={{ top: "always" }}>
-      <Text>{track.name}</Text>
-    </SafeAreaView>
+    <>
+      <Text style={{ fontSize: 24 }}>{tracks.name}</Text>
+      <MapView
+        initialRegion={{
+          longitudeDelta: 0.01,
+          latitudeDelta: 0.01,
+          ...initialCoords,
+        }}
+        style={styles.map}
+      >
+        <Polyline
+          coordinates={tracks.locations.map((location) => location.coords)}
+        />
+      </MapView>
+    </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 400,
+  },
+});
 
 export default TrackDetailScreen;
