3c1f0ae355db0bd38b8f003050df561918d6534d
ima17 <ima.sudaraka@outlook.com>
2023-03-27 00:06:24 +0530

    Refactor the code

--- src/navigationRefs.js
+++ src/navigationRefs.js
@@ -7,7 +7,6 @@ export const setNavigator = (nav) => {
 };
 
 export const navigate = (routeName, params) => {
-  console.log(navigator);
   navigator.dispatch(
     CommonActions.navigate({
       name: routeName,
