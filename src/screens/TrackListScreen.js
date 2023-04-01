a3152c27387365b361e776dba8e2cea83b9b9aad
ima17 <ima.sudaraka@outlook.com>
2023-04-01 20:45:58 +0530

    Implement loading indicator while fetch tracks

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {
   FlatList,
   TouchableOpacity,
   ScrollView,
   StyleSheet,
+  ActivityIndicator,
 } from "react-native";
 import { NavigationEvents } from "@react-navigation/compat";
 import { Context as TrackContext } from "../context/trackContext";
@@ -12,32 +13,49 @@ import { navigate } from "../navigationRefs";
 
 const TrackListScreen = () => {
   const { fetchTracks, state } = useContext(TrackContext);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchTracks().then(() => {
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <>
-      <NavigationEvents onWillFocus={() => fetchTracks()} />
-      <FlatList
-        data={state}
-        keyExtractor={(item) => item._id}
-        renderItem={({ item }) => {
-          return (
-            <ScrollView style={styles.listStyle}>
-              <TouchableOpacity
-                onPress={() => {
-                  navigate("TrackDetail", { _id: item._id, name: item.name });
-                }}
-              >
-                <ListItem>
-                  <ListItem.Content>
-                    <ListItem.Title>{item.name}</ListItem.Title>
-                  </ListItem.Content>
-                  <ListItem.Chevron />
-                </ListItem>
-              </TouchableOpacity>
-            </ScrollView>
-          );
+      <NavigationEvents
+        onWillFocus={() => {
+          console.log("fsds");
+          fetchTracks();
         }}
       />
+      {loading ? (
+        <ActivityIndicator size="large" style={styles.loadingIndicator} />
+      ) : (
+        <FlatList
+          data={state}
+          keyExtractor={(item) => item._id}
+          renderItem={({ item }) => {
+            return (
+              <ScrollView style={styles.listStyle}>
+                <TouchableOpacity
+                  onPress={() => {
+                    navigate("TrackDetail", { _id: item._id, name: item.name });
+                  }}
+                >
+                  <ListItem>
+                    <ListItem.Content>
+                      <ListItem.Title>{item.name}</ListItem.Title>
+                    </ListItem.Content>
+                    <ListItem.Chevron />
+                  </ListItem>
+                </TouchableOpacity>
+              </ScrollView>
+            );
+          }}
+        />
+      )}
     </>
   );
 };
@@ -46,6 +64,11 @@ const styles = StyleSheet.create({
   listStyle: {
     marginTop: 10,
     marginHorizontal: 10,
+  },
+  loadingIndicator: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
